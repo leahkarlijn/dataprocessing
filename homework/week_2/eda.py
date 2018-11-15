409dcde0c8c405a88f1cc1db866a1f52d946352d
leahkarlijn <leahkarlijn@hotmail.com>
2018-11-15 12:54:00 +0100

    hw week 2

--- homework/week_2/eda.py
+++ homework/week_2/eda.py
@@ -1,16 +1,59 @@
 import csv
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 input_csv = "input.csv"
+GDP = 'GDP ($ per capita) dollars'
+IM = 'Infant mortality (per 1000 births)'
 
-def get_data(filename):
-    with open(input_csv, 'r') as csvFile:
-        reader = csv.reader(csvFile)
-        # skip empty rows
-        for row in reader:
-            if row:
-                print(row)
-                # check for left out data (google) 
+def clean():
+    df = pd.read_csv(input_csv)
+    df = df.dropna()
+    df = df[df[GDP] != 'unknown']
+    return df
+
+def get_std(dataframe):
+    df[GDP] = df[GDP].str.strip(' dollars')
+    df[GDP] = pd.to_numeric(df[GDP])
+    std = df[GDP].std()
+
+def histo(dataframe):
+    df2 = df[np.abs(df[GDP]-df[GDP].mean()) <= (3*df[GDP].std())]
+    hist = df2[GDP].hist(bins=50)
+    plt.xlabel('GDP (dollars)')
+    plt.ylabel('std')
+    plt.title('GPD histo')
+    plt.show()
+
+def boxplot(dataframe):
+    IM_list = []
+    box_list = []
+    for x in df[IM]:
+        if ',' in x:
+            x = x.replace(',', '.')
+            x = float(x)
+            IM_list.append(x)
+        else:
+            x = float(x)
+            IM_list.append(x)
+    IMp = np.array(IM_list)
+    minimum = min(IM_list)
+    box_list.append(minimum)
+    firstq = np.percentile(IMp, 25)
+    box_list.append(firstq)
+    median = np.median(IMp)
+    box_list.append(median)
+    thirdq = np.percentile(IMp, 75)
+    box_list.append(thirdq)
+    maximum = max(IM_list)
+    box_list.append(maximum)
+
+    plt.boxplot(box_list)
+    plt.show()
 
 if __name__ == '__main__':
-
-    data = get_data(input_csv)
+    df = clean()
+    std = get_std(df)
+    histo = histo(df)
+    box = boxplot(df)
